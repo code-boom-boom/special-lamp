e89c40dadb001b123848dae2f6ea6d12c23d8da6
Love Coding <fedde198212@gmail.com>
2021-08-04 13:16:53 -0400

    style added, before webpack

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+/**
+ * Main App
+ */
 import React from "react";
 import { Provider } from "react-redux";
 import { configureStore } from "./store";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import MomentUtils from "@date-io/moment";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// CSS
+// import "./lib/reactifyCss";
+
+// app component
 import App from "./container/App";
 
 const MainApp = () => (
